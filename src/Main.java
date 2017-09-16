c65d6b8148d61f69452b021d77f27ae7027944a4
Manisha-Lakshmi <manishasathi21@gmail.com>
2017-09-16 11:32:13 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+	public static void main(String[] args) {
+		Student s=new Student(1,"abc",1996-06-21,80);
+		
+		StudentGroup sg=new StudentGroup(5);
+                                          
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+	}
+
+}
